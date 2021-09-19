58d4f1186a8b992cbb0f4c36b96db7577615dd83
Boris <55884200+boris-bk@users.noreply.github.com>
2021-09-19 21:31:23 +0300

    v1

---
+++ js/burger.js
@@ -0,0 +1,6 @@
+document.querySelector('.menu-icon-wrapper').onclick = function() {
+    document.querySelector('.menu-icon').classList.toggle('menu-icon-active');
+    document.querySelector('.menu-nav').classList.toggle('show');
+};
+
+
